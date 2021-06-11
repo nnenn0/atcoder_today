7bfc92da5aa746ac5569ade4e07591a6729277e7
nnenn0 <nnenn0memo@gmail.com>
2021-06-12 00:10:42 +0900

    add 詳細追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "Atcoder Today",
+    version = "1.0.0",
+    author = "nnenn0",
+    about = "Make a suggestion for today's contest"
+)]
+
 fn main() {
     println!("Hello, world!");
 }
